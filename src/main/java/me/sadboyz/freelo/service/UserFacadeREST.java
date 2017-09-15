f97896c7799762f7b063243e11800989459450d8
Hugo Bullon <hugobullont@gmail.com>
2017-09-14 20:45:26 -0500

    002 Eliminar EtiquetasXML - UserFacadeREST.java

--- src/main/java/me/sadboyz/freelo/service/UserFacadeREST.java
+++ src/main/java/me/sadboyz/freelo/service/UserFacadeREST.java
@@ -37,14 +37,14 @@ public class UserFacadeREST extends AbstractFacade<User> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(User entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, User entity) {
         super.edit(entity);
     }
