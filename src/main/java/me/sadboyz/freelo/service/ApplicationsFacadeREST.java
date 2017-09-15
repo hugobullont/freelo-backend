73c966db7cd1a95f5ba5ab651503d18140d58c76
ARCTURUS <ARCTURUS@ARTURO-PC>
2017-09-14 22:17:34 -0500

    Eliminadas EtiquetasXML: ApplicationsFacadeREST.java TransactionFacadeREST.java

--- src/main/java/me/sadboyz/freelo/service/ApplicationsFacadeREST.java
+++ src/main/java/me/sadboyz/freelo/service/ApplicationsFacadeREST.java
@@ -37,14 +37,14 @@ public class ApplicationsFacadeREST extends AbstractFacade<Applications> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(Applications entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Applications entity) {
         super.edit(entity);
     }
@@ -57,21 +57,21 @@ public class ApplicationsFacadeREST extends AbstractFacade<Applications> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public Applications find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Applications> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Applications> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
